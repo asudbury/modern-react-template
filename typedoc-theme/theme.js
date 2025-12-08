b9bdb1bce5c275b3519a0261f27edd9663316203
Adrian Sudbury <sudburya@gmail.com>
2025-12-08 09:28:11 +0000

    feat: use custom theme for typdoc/jsdoc - work in progress

---
+++ typedoc-theme/theme.js
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'typedoc';
+
+/**
+ * Custom TypeDoc theme that removes default items.
+ */
+export class CustomTheme extends DefaultTheme {
+  // We rely on a tiny CSS override to hide the footer element.
+}
